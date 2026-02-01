76dcaf3089708ba5b73bb0b795da4b0dcde72493
bsissou <51749750+bsissou@users.noreply.github.com>
2026-02-01 20:48:56 +0100

    Add Node.js presence check to script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,9 @@
-echo "Début du script"
+echo "Vérification de Node"
+
+if ! command -v node >/dev/null 2>&1; then
+  echo "Node n'est PAS installé"
+  exit 1
+fi
+
+echo "Node est présent"
 node -v
-echo "Fin du script"
