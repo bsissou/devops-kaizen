3dbc85bec787939cedfc9d312e9c4940219410bf
bsissou <51749750+bsissou@users.noreply.github.com>
2026-02-04 12:32:43 +0100

    Change build script to test script
    Updated script to reflect test execution instead of build process.

--- script.sh
+++ script.sh
@@ -1,8 +1,21 @@
-echo "Début du build"
+echo "Début des tests"
 
-echo "Build réalisé le $(date)" > build.txt
+RESULT=5
 
-echo "Contenu du fichier généré :"
-cat build.txt
+if [ "$RESULT" -eq 5 ]; then
+  echo "Test réussi"
+else
+  echo "Test échoué"
+  exit 1
+fi
 
-echo "Fin du build"
+echo "Fin des tests"
+
+#echo "Début du build"
+
+#echo "Build réalisé le $(date)" > build.txt
+
+#echo "Contenu du fichier généré :"
+#cat build.txt
+
+#echo "Fin du build"
